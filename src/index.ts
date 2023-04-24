590d0ebb946c70c5ae8eea72424e8e64d91a0846
Abri Grobler <grobleraf@gmail.com>
2023-04-24 19:41:10 +0200

    Better indexed exports

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+//Layout
+export { default as Grid } from './components/layout/grid';
+export { default as Flex } from './components/layout/flex';
+export { default as Box } from './components/layout/box';
+//Calendar
+export { Calendar } from './components/calendar/index';
+//Dropdown
+export { default as Dropdown } from './components/dropdown/index';
+//Dropzone
+export { default as Dropzone } from './components/dropzone/index';
+export { default as ImageDropzone } from './components/image-dropzone';
+//Elements
+export { default as Button } from './components/elements/button';
+export { default as ExternalLink } from './components/elements/external-link';
+export { default as Image } from './components/elements/image';
+export { default as Modal } from './components/elements/modal';
+export { default as TextField } from './components/elements/text-field';
+//Form
+export { default as Form } from './components/form/index';
+export { InputField, Checkbox } from './components/form/input';
+//Outside Alerter
+export { default as OutsideAlerter } from './components/outside-alerter';
+//Text
+export { Text as Paragraph } from './components/typography';
